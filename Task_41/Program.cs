177368d6ca0fa4e51329e2b9666f351afd378729
Ruslan <89604587433@mail.ru>
2022-09-18 16:05:01 +0300

    ользователь вводит с клавиатуры M чисел. осчитайте, сколько чисел больше 0 ввёл пользователь.

---
+++ Task_41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Метод подсчитывает количество положительных чисел
+int Calc(double[] array)
+{
+int count =0;
+for (int i=0; i<array.Length; i++)
+    {
+        if (array[i]>0) count++;
+    }
+    return count;
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите количество чисел : ");
+try
+{
+int quantity=Math.Abs(Convert.ToInt32(Console.ReadLine()));
+double[] array=new double[quantity];
+Console.WriteLine();
+for (int i=0; i<quantity; i++)
+    {
+        Console.Write($"{i+1})= ");
+        array[i]=Convert.ToDouble(Console.ReadLine());
+
+    }
+
+Console.WriteLine($"Количество положительных чисел : {Calc(array)}");
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
